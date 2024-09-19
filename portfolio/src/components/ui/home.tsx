b1a91dc883ee463a84ff5fc4c15888e8825f1b89
Jay Abegglen <jsabegglen96@gmail.com>
2024-09-19 00:42:12 -0700

    setup(root): setup initial webpage

---
+++ portfolio/src/components/ui/home.tsx
@@ -0,0 +1,14 @@
+"use client";
+export const Home = () => {
+  return (
+    <div>
+      <div className="pt-32 pb-16 md:pt-52 md:pb-32 relative max-w-5xl min-h-screen px-4 mx-auto sm:px-6 space-y-80">
+        {/* Hero */}
+        {/* Name and background */}
+        {/* Description of Experience */}
+        {/* Ig-posts */}
+      </div>
+      {/* Footer */}
+    </div>
+  );
+};
